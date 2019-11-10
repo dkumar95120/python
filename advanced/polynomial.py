e6c9a78a67499d2079f6fd23bc2a70db46ddc4f6
Dharmendra Kumar <dhakumar@hotmail.com>
2019-11-09 16:48:13 -0800

    Added str method for polinomial and fixed add method

--- advanced/polynomial.py
+++ advanced/polynomial.py
@@ -6,20 +6,37 @@
 #  x()    -> __call__
 
 class Polynomial:
-	def __init__ (self, *coeffs):
-		self.coeffs = coeffs
+    def __init__ (self, *coeffs):
+        self.coeffs = coeffs
 
-	def __repr__(self):
-		return 'Polynomial(*{!r})'.format(self.coeffs)
+    def __repr__(self):
+        return 'Polynomial(*{!r})'.format(self.coeffs)
 
-	def __add__(self, other):
-		return Polynomial(*(x + y for x, y in zip (self.coeffs, other.coeffs)))
+    def __add__(self, other):
+        coeffs = list(self.coeffs) if len(self.coeffs) > len(other.coeffs) else list(other.coeffs)
+        for i in range(min(len(self.coeffs),len(other.coeffs))):
+            coeffs[-(i+1)] = self.coeffs[-(i+1)] + other.coeffs[-(i+1)]
 
-	def __len__(self):
-		return len(self.coeffs)
+        return Polynomial(*coeffs)
 
-	def __call__(self):
-		pass
+    def __len__(self):
+        return len(self.coeffs)
+
+    def __call__(self):
+        pass
+        
+    def __str__(self):
+        order = len(self.coeffs) - 1
+        s =''
+        for i, c in enumerate(self.coeffs):
+            str_c = str(c) if c > 1 else ''
+            if order - i > 1:
+               s += str_c + 'x' + str(order-i) + ' + '
+            elif order - i == 1:
+               s += str_c + 'x' + ' + '
+            elif order - i == 0:
+               s += str(c) 
+        return s
 
 p1 = Polynomial(1, 2, 3) # x2 + 2x + 3
-p2 = Polynomial(3, 4, 3) # 3x2 + 4x + 3
+p2 = Polynomial(3, 1, 1) # 3x2 + 4x + 3
