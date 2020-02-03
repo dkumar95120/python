2a35458c0bb2459633d3e0a6c5ff73e2d2cdee9f
dkumar95120 <dkumar@gmail.com>
2020-02-02 21:55:57 -0800

    Modified to make vehicle a class

--- exercises/parking_lot.py
+++ exercises/parking_lot.py
@@ -1,8 +1,15 @@
-class ParkingSpot (object):
+from collections import defaultdict
+class Vehicle:
+    def __init__(self, id, type, stype):
+        self.id = id  # license plate
+        self.type = type # [Bus], [Car], Two-Wheeler [TW]
+        self.stype = stype # [H]andicapped, [C]ompact, [R]egular
+
+class ParkingSpot:
     def __init__(self, number, stype, size, location, valet=False):
         self.stype    = stype # e.g. [H]andicapped, [C]ompact, [R]egular
         self.size     = size  # e.g. [S]mall, [M]edium, [L]arge
-        self.vehicle  = None # e.g.[Bus], [Car], Two-Wheeler [TW]
+        self.vehicle  = None 
         self.number   = number
         self.valet    = valet   # True, False
         self.location = location # e.g. [F]ront, [B]ack, or [A]ny
@@ -15,77 +22,81 @@ class ParkingSpot (object):
         self.vehicle = None
         return vehicle
 
-    def can_park(self, vehicle='Car', stype='R', valet=False):
+    def can_park(self, vehicle, valet=False):
         if valet != self.valet: # cannot park in a valet spot if not valet or vice versa
             return False
 
-        if self.vehicle != None: # spot is already filled
+        if self.stype == 'H' and vehicle.stype != 'H': # Regular or Compact vehicle cannot part in a handicapped spot
             return False
 
-        if self.stype == 'H' and stype != 'H': # Regular or Compact vehicle cannot part in a handicapped spot
-            return False
-
-        if self.stype == 'C' and stype == 'R': # cannot park a regular vehicle in a compact spot
+        if self.stype == 'C' and vehicle.stype != 'C': # cannot park a regular vehicle in a compact spot
             return False
 
         # check if the vehicle can fit in the spot
-        if (vehicle == 'Car' and self.size == 'S') or (vehicle == 'Bus' and self.size != 'L'):
+        if (vehicle.type == 'Car' and self.size == 'S') or (vehicle.type == 'Bus' and self.size != 'L'):
             return False
 
         return True
 
 class ParkingLot(object):
     def __init__(self):
-        self.lot = []
+        self.lot = defaultdict(list)
         self.max_spot = 0
 
     def add_spot (self, stype, size, location, valet=False):
         self.max_spot += 1
         spot = ParkingSpot(self.max_spot, stype, size, location, valet)
-        self.lot.append(spot)
+        self.lot[None].append(spot)
 
     def available_spots(self):
         nspot = {}
         nspot['S'] = []
         nspot['M'] = []
         nspot['L'] = []
-        for spot in self.lot:
-            if spot.vehicle == None:
-                nspot[spot.size].append(spot.number)
+        for spot in self.lot[None]:
+            nspot[spot.size].append(spot.number)
         return nspot
 
-    def park(self, vehicle, stype, location, valet=False): # assuming vehicle can be parked in Any location
+    def park(self, vehicle, location, valet=False): # assuming vehicle can be parked in Any location
         parked = False
         area={}
         area['F'] = 'Front'
         area['B'] = 'Back'
-        for spot in self.lot:
-            if spot.can_park(vehicle, stype, valet):
+        for spot in self.lot[None]:
+            if spot.can_park(vehicle, valet):
                 if location == spot.location:
                     spot.park(vehicle)
-                    print(vehicle, " parked in spot", spot.number, 'in ', area[spot.location])
+                    print(vehicle.id, " parked in spot", spot.number, 'in ', area[spot.location])
                     parked = True
                     break
         if not parked: # try any location if cannot park in preferred location
-            for spot in self.lot:
-                if spot.can_park(vehicle, stype, valet):
+            for spot in self.lot[None]:
+                if spot.can_park(vehicle, valet):
                     spot.park(vehicle)
-                    print(vehicle, " parked in spot", spot.number, 'in ', area[spot.location])
+                    print(vehicle.id, " parked in spot", spot.number, 'in ', area[spot.location])
                     parked = True
                     break
 
         if not parked:
-            print('\nSorry, no space available for', vehicle,'\n')
+            print('\nSorry, no space available for', vehicle.id,'\n')
+        else:
+            self.lot[vehicle.id] = self.lot[None].pop(self.lot[None].index(spot))
 
         return parked
 
-    def unpark(self, number):
-        vehicle = None
-        for spot in self.lot:
-            if spot.number == number:
-                vehicle = spot.unpark()
-                break
-        return vehicle
+    def unpark(self, vehicle_id):
+        spot = None
+        if vehicle_id in self.lot:
+            spot = self.lot[vehicle_id]
+            spot.unpark()
+            # add this spot to available spots
+            self.lot[None].append(spot)
+            # delete reference to this vehicle in this parking lot
+            del self.lot[vehicle_id]
+        else:
+            print(f"{vehicle_id} is not parked in this parking lot")
+
+        return spot
 
     def status(self):
         nspot = self.available_spots()
@@ -97,13 +108,13 @@ class ParkingLot(object):
 
 # driver for parking lot class dry run
 parking_lot = ParkingLot()
-# add 5 large spaces and 5 handicapped spaces in Front
+# add large spaces for buses and for handicapped vehicles in Front
 big = 5
 for i in range(big):
     parking_lot.add_spot(stype='R', size='L', location='F', valet=False)
     parking_lot.add_spot(stype='H', size='L', location='F', valet=False)
 
-# add 20 small spaces (10 in front and 10 in back) for two wheelers and compact cars
+# add spaces for two wheelers and compact cars (half in front and the other half in back)
 sml = 20
 for i in range(sml):
     if i < sml//2:
@@ -111,8 +122,8 @@ for i in range(sml):
     else:
         parking_lot.add_spot(stype='C', size='S', location='B', valet=False)
 
-# add 40 spaces 20 in front 20 in back for Regular Mid size cars
-mid = 40
+# add spaces for Regular Mid size cars half in front and the other half in back 
+mid = 30
 for i in range(mid):
     if i < mid//2:
         parking_lot.add_spot(stype='R', size='M', location='F', valet=False)
@@ -122,17 +133,27 @@ for i in range(mid):
 parking_lot.status()
 # park six buses 
 for i in range(big+1):
-    parking_lot.park(vehicle='Bus', stype='R', location='F', valet=False)
+    vehicle = Vehicle(f'4WRK45{i}', 'Bus', 'R')
+    parking_lot.park(vehicle, location='F', valet=False)
 
 for i in range(mid+1):
-    parking_lot.park(vehicle='Car', stype='R', location='F', valet=False)
+    if i < 5:
+        stype = 'H'
+    elif i < 20:
+        stype = 'C'
+    else:
+        stype = 'R'
+    vehicle = Vehicle(f'5XHT8{i:02}', 'Car', stype)
+    parking_lot.park(vehicle, location='F', valet=False)
 
 for i in range(sml+1):
-    parking_lot.park(vehicle='TW', stype='C', location='F', valet=False)
+    vehicle = Vehicle(f'7SFM9{i:02}', 'TW', 'C')
+    parking_lot.park(vehicle, location='F', valet=False)
 
 parking_lot.status()
 
-for number in range(9,70,10):
-    vehicle = parking_lot.unpark(number)
-    print('Spot {} is now available with a {} moving out'.format(number, vehicle))
+for vehicle_id in ['4WRK450','4WRK451', '5XHT800', '5XHT801', '7SFM905' '7SFM917']:
+    spot = parking_lot.unpark(vehicle_id)
+    if spot:
+        print(f'Spot {spot.number} is now available with a {vehicle_id} moving out')
     parking_lot.status()
